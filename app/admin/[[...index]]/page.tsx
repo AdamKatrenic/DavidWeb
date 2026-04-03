7c5444c4097a4d7cea60444819251bf112185a47
AdamKatrenic <adamkatrenic@gmail.com>
2026-04-03 21:20:55 +0200

    skrytie navbaru v admine

--- app/admin/[[...index]]/page.tsx
+++ app/admin/[[...index]]/page.tsx
@@ -6,8 +6,26 @@ import config from '../../../studio/sanity.config'
 
 export default function AdminPage() {
   return (
-    <div className="min-h-screen">
-      <NextStudio config={config as unknown as Config} />
-    </div>
+    <>
+      <style jsx global>{`
+        nav, 
+        header, 
+        footer, 
+        .your-navbar-class, 
+        .your-footer-class { 
+          display: none !important; 
+        }
+        
+        /* Toto zabezpečí, aby Sanity Studio využilo celú výšku obrazovky */
+        body {
+          margin: 0 !important;
+          padding: 0 !important;
+        }
+      `}</style>
+
+      <div className="min-h-screen">
+        <NextStudio config={config as unknown as Config} />
+      </div>
+    </>
   )
 }
